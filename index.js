a28856a2f359be114ca31a89c592337d63cea6b1
bvalette <bvalette@student.42.fr>
2022-03-17 16:36:11 +0100

    done

--- index.js
+++ index.js
@@ -3,9 +3,31 @@ const mongoose = require('mongoose');
 // Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
+
 const data = require('./data');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+
+const lvl = Recipe.schema.path('level').enumValues;
+const dishTypes = Recipe.schema.path('dishType').enumValues;
+
+async function logAllTitles() {
+  const allRecipes = await Recipe.find({});
+  console.log('↪️ -------------- All Recipes:');
+  allRecipes.forEach(r => console.log(`${r.title} -> ${r.duration}`))
+}
+
+
+const myRecipe =
+  {
+    title: 'Flan aux huitres 👌',
+    level: lvl[0],
+    ingredients: ['patates', 'ail', 'huitres'],
+    cuisine: 'froncaiiise',
+    dishType: dishTypes[3],
+    duration: 42,
+  }
+
 
 // Connection to the database "recipe-app"
 mongoose
@@ -15,9 +37,14 @@ mongoose
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
+  .then(() => Recipe.create(myRecipe))
+  .then(() => Recipe.insertMany(data))
+  .then(() => logAllTitles())
+  .then(() => Recipe.findOneAndUpdate({title: 'Rigatoni alla Genovese'}, {duration: 100}, {new: true}))
+  .then(() => logAllTitles())
+  .then(() => Recipe.deleteOne({title: 'Carrot Cake'}))
+  .then(() => logAllTitles())
+  .then(() => mongoose.connection.close())
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
